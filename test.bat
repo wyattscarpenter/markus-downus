cf9b90816e1ec34e154d62d47cfb64ca31c1c02a
wyattscarpenter <wyattscarpenter@gmail.com>
2024-08-29 01:16:45 -0700

    rename files, also directly compare json values

--- test.bat
+++ test.bat
@@ -1,4 +1,5 @@
-python markus-downus.py example.md >result.txt
-wsl sdiff --ignore-all-space test.json result.txt
+python markus-downus.py test-input.md >test-result.json
+wsl sdiff --ignore-all-space test-expected.json test-result.json
+python -c "import json; print('Jsons match.') if ( json.load(open('test-expected.json')) == json.load(open('test-result.json')) ) else print('Jsons do NOT match.')"
 mypy --strict markus-downus.py --enable-incomplete-feature=NewGenericSyntax --pretty
 pyright markus-downus.py
